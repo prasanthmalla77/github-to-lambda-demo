4322ad08071c8f190b92bcaf8e3f2bbd55f0c77f
prasanthmalla77 <prasanthmalla77@gmail.com>
2024-01-08 23:12:45 +0530

    j

--- lambda_function.py
+++ lambda_function.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 def lambda_handler(event, context):
     d = {'col1': [1, 2], 'col2': [3, 4]}
-    df = pd.dataframe(data=d)
+    df = pd.DataFrame(data=d)
     print(df)
     print('Done x')
